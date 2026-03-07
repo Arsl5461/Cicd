419a3d2fe97379c2d987d135cf2b7f63e81f3cb4
pny <pnymeet@gmail.com>
2026-03-07 15:56:53 +0500

    updated code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app=express()
-const PORT=5005
+const PORT=5005;
+console.log("test")
 
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
