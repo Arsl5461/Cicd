8ea820286f32730b28f1f8e408498d7f0ae6176d
Arsl5461 <arxlan62@gmail.com>
2026-03-14 15:45:26 +0500

    added first route

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express=require("express")
 const app=express()
 const PORT=5005;
-console.log("tessdfsdjjjftsadfdsdf")
+
+app.listen("/",(req,res)=>{
+    res.send("Hello World")
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
