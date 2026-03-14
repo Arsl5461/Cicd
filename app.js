a3275f71deddd9c2e2644532c480070ec30d95b3
Arsl5461 <arxlan62@gmail.com>
2026-03-14 15:55:18 +0500

    updated code

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.use("/",(req,res)=>{
 app.use("/health",(req,res)=>{
     res.json({message:"Hello World From PNY"})
 })
+
+app.use("/test",(req,res)=>{
+    res.json({message:"Hello World From PNY it is a test"})
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
 })
